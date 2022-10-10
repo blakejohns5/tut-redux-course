d26042eb11a9adbeb930dbbd48884b55adfc28b0
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-10-10 10:02:15 +0200

    reset rest of file structure for beginning new project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-import './App.css';
-import AddPostForm from './features/posts/AddPostForm';
-import PostsList from './features/posts/PostsList';
-import SinglePostPage from './features/posts/SinglePostPage';
-import EditPostForm from './features/posts/EditPostForm'
-import Layout from './components/Layout';
-import UsersList from './features/users/UsersList';
-import UserPage from './features/users/UserPage';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import TodoList from './features/todos/TodoList';
 
 
-function App() {
+
+export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        
-        <Route index element={<PostsList />} />
-
-        <Route path="post">
-          <Route index element={<AddPostForm  />} />
-          <Route path=":postId" element={<SinglePostPage />} />
-          <Route path="edit/:postId" element={<EditPostForm />} />
-        </Route>
-
-        <Route path="user">
-          <Route index element={<UsersList  />} />
-          <Route path=":userId" element={<UserPage />} />
-        </Route>
-
-        <Route path="*" element={<Navigate to="/" replace />} />
-
-      </Route>
-    </Routes> 
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import postsReducer from '../features/posts/postsSlice'
-import usersReducer from '../features/users/usersSlice'
+
 
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer,
-    users: usersReducer,
+
   }
 })
